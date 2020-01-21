a6577c3b00a6c191f48bf15aff92b711c93f3841
Bryn Heath <brynheath1@gmail.com>
2020-01-21 21:05:07 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -20,3 +20,12 @@ i=0
   return arr
 end
 ##########################################################
+def map_to_double(source_array)
+  arr = []
+  i=0
+    while i < source_array.size do
+      arr << source_array[i] * 2
+      i += 1
+    end
+    return arr
+  end
