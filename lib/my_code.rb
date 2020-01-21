3176521c270e5ba951e1ae8a2c4e237073fa6aa2
Bryn Heath <brynheath1@gmail.com>
2020-01-21 21:07:45 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -41,4 +41,4 @@ def map_to_square(source_array)
     return arr
   end
 ##########################################################
-reduce_to_total(source_array, starting_point)
+def reduce_to_total(source_array, starting_point)
