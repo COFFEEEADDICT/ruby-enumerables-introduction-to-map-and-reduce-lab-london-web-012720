68d767c7034d4af3d5120c38b00185b323fb8c9e
Bryn Heath <brynheath1@gmail.com>
2020-01-21 21:45:33 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -63,7 +63,6 @@ def reduce_to_total(source_array, starting_point=0)
   end
   ##########################################################
 def reduce_to_all_true(source_array)
-  pp source_array
   source_array.length.times { |i|
     if !source_array[i]
       return false
