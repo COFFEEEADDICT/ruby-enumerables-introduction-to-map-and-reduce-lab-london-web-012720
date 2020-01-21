7f12dd6fdf4b23f0e35a06f74498d3e033ecb3a0
Bryn Heath <brynheath1@gmail.com>
2020-01-21 21:43:06 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -55,7 +55,7 @@ def reduce_to_total(source_array, starting_point)
     return add
   end
 ##########################################################
-def reduce_to_total(source_array, starting_point)
+def reduce_to_total(source_array, starting_point=0)
   total = starting_point
     source_array.length.times do |index|
       total = source_array[index] + total end
