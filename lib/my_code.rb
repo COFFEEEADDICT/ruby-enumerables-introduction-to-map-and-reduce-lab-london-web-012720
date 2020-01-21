3fb6cb3d67cfc68fa1418f138ac3a467fcfe8920
Bryn Heath <brynheath1@gmail.com>
2020-01-21 21:57:27 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -54,7 +54,7 @@ def map_to_square(source_array)
 #     return add
 #   end
 ##########################################################
-def reduce_to_total(source_array, starting_point)
+def reduce_to_total(source_array, starting_point=10)
   total = starting_point
     source_array.length.times do |i|
       total = source_array[i] + total end
