0d1525f94079c7704d18e60182526209767b3f95
Bryn Heath <brynheath1@gmail.com>
2020-01-21 21:04:20 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,3 +8,15 @@ i=0
   end
   return arr
 end
+
+##########################################################
+def map_to_negativize(source_array)
+arr = []
+i=0
+  while i < source_array.size do
+    arr << -1 * source_array[i]
+    i += 1
+  end
+  return arr
+end
+##########################################################
