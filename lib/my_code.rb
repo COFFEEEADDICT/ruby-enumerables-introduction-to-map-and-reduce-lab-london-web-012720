6c6fab413d2982926547bd50bc6f0135abef1083
Bryn Heath <brynheath1@gmail.com>
2020-01-21 21:10:44 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -45,7 +45,8 @@ def reduce_to_total(source_array, starting_point)
   arr = []
   i=0
     while i < source_array.size do
-      arr += source_array[i]
+      arr = starting_point + source_array[i]
+      total += arr[i]
       i += 1
     end
     return arr
